65876a457ada29d4009343b1042b124ed5791055
Phil Toms <pip.asqueek@hotmail.co.uk>
2017-05-20 18:00:39 +0100

    map a new output id accumulator onto the object

---
+++ src/mapOutputId.js
@@ -0,0 +1,9 @@
+const fp = require('lodash/fp');
+
+// map a new output id accumulator onto the object.
+// These props will be used to construct the answer keys later on
+const mapOutputId = (oid, id) => obj => Object.assign({}, obj, {
+  outputId: fp.flatten([oid, id].filter(fp.identity))
+});
+
+module.exports = mapOutputId;
--- test/flattenTree.js
+++ test/flattenTree.js
@@ -4,6 +4,7 @@ const questionset = require('../examples/questionset.json');
 const answers = require('../examples/answers.json');
 const expectedOutput = require('../examples/expectedOutput.json');
 
+const mapOutputId = require('../src/mapOutputId');
 const flatmap = require('../src/flatmap');
 const flattenTree = require('../lib/flattenTree');
 
@@ -24,17 +25,25 @@ const tree = [{
   }]
 }];
 
+test('map a new output id accumulator onto the object', t => {
+  t.deepEqual(mapOutputId([1], 2)({}).outputId, [1, 2])
+});
+
+test('ensure normalised output ids', t => {
+  t.deepEqual(mapOutputId([1], undefined)({}).outputId, [1])
+});
+
 test('flatmap the tree and preserve order', t => {
-  t.deepEqual(flatmap(tree).map(({id, trigger}) => id || trigger), [1, 2, 3, 4, 5, 6])
-})
+  t.deepEqual(flatmap(tree).map(({id, trigger}) => id || trigger), [1, 2, 3, 4, 5, 6]);
+});
 
 test('flattens correctly the provided examples', t => {
-  const result = flattenTree(questionset, answers)
+  const result = flattenTree(questionset, answers);
 
   t.is(result.length, expectedOutput.length);
 
   //result[0] = Object.assign({}, result[0], {bust: true})
 
   // iterate the expectation to highlight specific fails..
-  fp.zipWith((r, e) => t.deepEqual(r, e), result, expectedOutput)
+  fp.zipWith((r, e) => t.deepEqual(r, e), result, expectedOutput);
 });
