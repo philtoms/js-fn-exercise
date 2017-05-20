f4449117308042ac928e098513f2b936286dc404
Phil Toms <pip.asqueek@hotmail.co.uk>
2017-05-20 10:31:48 +0100

    first commit - install and establish target expectation

--- test/flattenTree.js
+++ test/flattenTree.js
@@ -1,3 +1,17 @@
 const test = require('ava');
+const fp = require('lodash/fp');
+const questionset = require('../examples/questionset.json');
+const answers = require('../examples/answers.json');
+const expectedOutput = require('../examples/expectedOutput.json');
+const flattenTree = require('../lib/flattenTree.js');
 
-test.todo('flattens correctly the provided examples');
+test('flattens correctly the provided examples', t => {
+  const result = flattenTree(questionset, answers)
+
+  t.is(result.length, expectedOutput.length);
+
+  //result[0] = Object.assign({}, result[0], {bust: true})
+
+  // iterate the expectation to highlight specific fails..
+  fp.zipWith((r, e) => console.log(r, e) || t.deepEqual(r, e), result, expectedOutput)
+});
